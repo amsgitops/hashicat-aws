bba0208a02c8257ac85902461c3a5c3943c8e079
CodeKeeper <codekeeper@amazon.com>
2026-05-13 23:53:44 +0000

    CodeKeeper: IaC modification

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "catapp_url" {
 output "catapp_ip" {
   value = "http://${aws_eip.hashicat.public_ip}"
 }
+
+output "sns_topic_arn" {
+  description = "ARN of the codekeeper-test-alerts SNS topic."
+  value       = aws_sns_topic.codekeeper_test_alerts.arn
+}
